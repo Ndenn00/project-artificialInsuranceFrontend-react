9872ce7f50ec4efe6cdc0aee909009ab149f063f
Nial Dennison <ndennison01@qub.ac.uk>
2019-04-02 22:37:04 +0100

    Navbar and Home added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import PersonalDetails from './components/personalDetails'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import PersonalDetails from './components/PersonalDetails'
 
 import {BrowserRouter, Route, Switch} from 'react-router-dom'; 
 
 class App extends Component {
   render() {
     return (  
-      <BrowserRouter>  
+      <BrowserRouter> 
+      <Navbar/> 
           <Switch>
-            <Route path="/personaldetails" component={PersonalDetails} exact/>
+          <Route path="/" component={Home} exact/>
+
+            <Route path="/quotepersonaldetails" component={PersonalDetails} exact/>
           </Switch>
       </BrowserRouter>
     );
---
+++ src/components/Home.js
@@ -0,0 +1,20 @@
+import React from "react"; 
+
+class PersonalDetails extends React.Component {
+  
+    render() {
+      return (
+            <div className="section no-pad-bot">
+                <div className="container">
+                    <h1 className="header center orange-text">Welcome Traveller</h1>
+                </div>
+                <div className="row center">
+                    <h5 class="header col s12 light">Why not get some sweet insurance?</h5>
+                </div>
+            </div>
+            
+      );
+    }
+  }
+
+  export default PersonalDetails; 
