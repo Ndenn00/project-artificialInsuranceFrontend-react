b029659779d9ea52b9f8226b4ac06fc5d67c1cca
Nial Dennison <nialdennison00@gmail.com>
2019-04-28 19:41:58 +0100

    vehicle details validation added

--- src/components/VehicleDetails.js
+++ src/components/VehicleDetails.js
@@ -4,7 +4,6 @@ import { Field } from "formik";
 import Banner from "./Banner";
 import FormErrors from "./FormErrors";
 
-
 const VehicleDetails = props => {
   return (
     <div className="container">
@@ -21,19 +20,13 @@ const VehicleDetailsForm = props => {
         <div className="six columns">
           <label for="vin">Vin Number</label>
           <Field className="u-full-width" type="text" name="vin" />
-          <FormErrors
-            errors={props.errors.vin}
-            touched={props.touched.vin}
-          />
+          <FormErrors errors={props.errors.vin} touched={props.touched.vin} />
         </div>
 
         <div className="six columns">
           <label for="reg">Registration No.</label>
           <Field className="u-full-width" type="text" name="reg" />
-          <FormErrors
-            errors={props.errors.reg}
-            touched={props.touched.reg}
-          />
+          <FormErrors errors={props.errors.reg} touched={props.touched.reg} />
         </div>
       </div>
 
@@ -41,11 +34,13 @@ const VehicleDetailsForm = props => {
         <div className="six columns">
           <label for="make">Make</label>
           <Field className="u-full-width" type="text" name="make" />
+          <FormErrors errors={props.errors.make} touched={props.touched.make} />
         </div>
 
         <div className="six columns">
           <label for="model">Model</label>
           <Field className="u-full-width" type="text" name="model" />
+          <FormErrors errors={props.errors.model} touched={props.touched.model} />
         </div>
       </div>
 
@@ -53,11 +48,14 @@ const VehicleDetailsForm = props => {
         <div className="six columns">
           <label for="year">Year</label>
           <Field className="u-full-width" type="text" name="year" />
+          <FormErrors errors={props.errors.year} touched={props.touched.year} />
+
         </div>
 
         <div className="six columns">
           <label for="miles">Miles</label>
           <Field className="u-full-width" type="text" name="miles" />
+          <FormErrors errors={props.errors.miles} touched={props.touched.miles} />
         </div>
       </div>
 
@@ -76,6 +74,8 @@ const VehicleDetailsForm = props => {
             type="text"
             name="lengthOfOwnership"
           />
+          <FormErrors errors={props.errors.lengthOfOwnership} touched={props.touched.lengthOfOwnership} />
+
         </div>
       </div>
       <br />
--- src/utility/ValidationSchema.js
+++ src/utility/ValidationSchema.js
@@ -2,6 +2,9 @@ import * as regex from "./Regex";
 import * as Yup from "yup";
 
 const maxDate = new Date(new Date().setFullYear(new Date().getFullYear() - 17));
+const currentYear = new Date().getFullYear();
+const minAge = currentYear-40;  
+console.log(minAge)
 
 export const validationSchema = Yup.object().shape({
     firstname: Yup.string()
@@ -22,6 +25,12 @@ export const validationSchema = Yup.object().shape({
       .required("Phone is required"),
     dob: Yup.date().max(maxDate, "You have to be at least 17 Years old").required("Date of birth is required") ,
     vin: Yup.string().matches(regex.vinRegexp, "Vin not valid").required('Vin is required'),
-    reg: Yup.string().matches(regex.registartionRegexp, "Registartion No. not valid").required("Registartion number required")
+    reg: Yup.string().matches(regex.registartionRegexp, "Registartion No. not valid").required("Registartion number required"),
+    make: Yup.string().min(3, "Not a valid make").required("Make is required"),
+    model: Yup.string().min(3, "Not a valid model").required("Model is required"),
+    year: Yup.number().min(minAge, "Not a valid year, too old").max(currentYear, "Not a valid year, that's in the future..").required("Year is required"),
+    miles: Yup.number().max(500000, "Not a valid milage").required("Mileage is required"),
+    lengthOfOwnership: Yup.number().min(0, "Not a valid length of ownership").max(50, "Not a valid length of ownership").required("Length of ownership is required"),
+
 
   });
